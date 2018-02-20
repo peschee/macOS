87a64810cf579d0c750ed5aa6cd88156bc4fc53c
Peter Siska <pesche@gridonic.ch>
2018-02-20 13:42:12 +0100

    Use `test` instead of `dev` for local urls. @see https://ma.ttias.be/chrome-force-dev-domains-https-via-preloaded-hsts/ @see https://tools.ietf.org/html/draft-west-let-localhost-be-localhost-06

--- install.sh
+++ install.sh
@@ -7,63 +7,63 @@ task() {
     printf "\n${headline} %s ${reset}\n" "$*"
 }
 
- task 'Install oh-my-zsh'
- sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ # task 'Install oh-my-zsh'
+ # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
- task 'Linking dotfiles…'
- ln -sfv $(pwd)/dotfiles/.zshrc $(pwd)/dotfiles/.zprofile ~
- ln -sfv $(pwd)/dotfiles/.gitconfig $(pwd)/dotfiles/.gitignore ~
- ln -sfv $(pwd)/dotfiles/.gemrc ~
- for file in $(pwd)/dotfiles/.{path,exports,aliases,functions,extra}; do
-     ln -sfv "$file" ~ && source "$file"
- done
- task 'Linking ~/bin…'
- ln -sfv $(pwd)/bin ~
+ # task 'Linking dotfiles…'
+ # ln -sfv $(pwd)/dotfiles/.zshrc $(pwd)/dotfiles/.zprofile ~
+ # ln -sfv $(pwd)/dotfiles/.gitconfig $(pwd)/dotfiles/.gitignore ~
+ # ln -sfv $(pwd)/dotfiles/.gemrc ~
+ # for file in $(pwd)/dotfiles/.{path,exports,aliases,functions,extra}; do
+ #     ln -sfv "$file" ~ && source "$file"
+ # done
+ # task 'Linking ~/bin…'
+ # ln -sfv $(pwd)/bin ~
 
- task 'Installing Homebrew…'
- /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ # task 'Installing Homebrew…'
+ # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
- task 'Installing Homebrew bundle…'
- brew tap Homebrew/bundle
- brew install mas
- brew bundle
+ # task 'Installing Homebrew bundle…'
+ # brew tap Homebrew/bundle
+ # brew install mas
+ # brew bundle
 
- task 'Setting up node…'
- npm install -g n npmbrew avn avn-n grunt-cli
- node -v
+ # task 'Setting up node…'
+ # npm install -g n npmbrew avn avn-n grunt-cli
+ # node -v
 
- task 'Installing rbenv…'
- git clone https://github.com/rbenv/rbenv.git ~/.rbenv
- git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
- rbenv install 2.3.1
- rbenv global 2.3.1
- ruby -v
+ # task 'Installing rbenv…'
+ # git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+ # git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+ # rbenv install 2.3.1
+ # rbenv global 2.3.1
+ # ruby -v
 
- task 'Installing Atom config…'
- mkdir ~/.atom
- ln -sfv $(pwd)/dotfiles/.atom/{config,keymap,snippest,toolbar}.cson ~/.atom
- ln -sfv $(pwd)/dotfiles/.atom/styles.less ~/.atom
+ # task 'Installing Atom config…'
+ # mkdir ~/.atom
+ # ln -sfv $(pwd)/dotfiles/.atom/{config,keymap,snippest,toolbar}.cson ~/.atom
+ # ln -sfv $(pwd)/dotfiles/.atom/styles.less ~/.atom
 
- task 'Installing Atom packages/themes…'
- apm install \
-     color-picker \
-     colornamer \
-     docblockr \
-     emmet \
-     file-icons \
-     flex-tool-bar
-     git-control \
-     highlight-selected \
-     html-head-snippets \
-     language-apache \
-     language-ini \
-     native-ui \
-     pigments \
-     pretty-json \
-     project-manager \
-     sort-lines \
-     todo-show \
-     tool-bar
+ # task 'Installing Atom packages/themes…'
+ # apm install \
+ #     color-picker \
+ #     colornamer \
+ #     docblockr \
+ #     emmet \
+ #     file-icons \
+ #     flex-tool-bar
+ #     git-control \
+ #     highlight-selected \
+ #     html-head-snippets \
+ #     language-apache \
+ #     language-ini \
+ #     native-ui \
+ #     pigments \
+ #     pretty-json \
+ #     project-manager \
+ #     sort-lines \
+ #     todo-show \
+ #     tool-bar
 
 task 'Installing (web) development setup…'
 ln -sfv $(pwd)/etc/dnsmasq.conf $(brew --prefix)/etc/
@@ -90,7 +90,7 @@ openssl req \
   -days 3650 \
   -nodes \
   -x509 \
-  -subj "/C=US/ST=State/L=City/O=Organization/OU=$(whoami)/CN=*.dev" \
+  -subj "/C=US/ST=State/L=City/O=Organization/OU=$(whoami)/CN=*.test" \
   -keyout ~/Sites/ssl/private.key \
   -out ~/Sites/ssl/selfsigned.crt
 
